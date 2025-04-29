39db23326bebe91698c97a1686ffba964c344365
tegarrramadhaaan <anakrama2000@gmail.com>
2025-04-29 11:13:14 +0700

    c

--- pertemuan7.cpp
+++ pertemuan7.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa{}
+class mahasiswa{
 
 public:
 int nim;
 string nama;
 float nilai;
 
+void print_date(){
+    cout << "NIM    = " << nim << endl;
+    cout << "Nama    = " << nama << endl;
+    cout << "Nilai    = " << nilai << endl;
+}
 
+
+
+};
